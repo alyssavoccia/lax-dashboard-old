19f3c01340075f5522c39bbe356f7ac57dc24b8b
Alyssa Voccia <avoccia@hotmail.com>
2022-01-03 06:47:38 -0500

    Updated high school profile page to update the link status on submission

--- src/components/profile-hs-link-grid/ProfileHsLinkGrid.js
+++ src/components/profile-hs-link-grid/ProfileHsLinkGrid.js
@@ -31,37 +31,40 @@ class ProfileHsLinkGrid extends React.Component {
     }
   }
 
-  render() {
-    const { agilityLink, broadLink, fortyLink, threeLink, verticalLink, wbLink, wbLinkExpires, threeLinkExpires, broadLinkExpires, verticalLinkExpires, agilityLinkExpires, fortyLinkExpires } = this.state;
-    const currentUser = this.props.currentUser;
+  handleSubmit = (e) => {
+    // Get the input id to know which value is being updated in firebase
+    const inputId = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].id;
 
-    function handleSubmit(e) {
-      // Get the input id to know which value is being updated in firebase
-      const inputId = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].id;
+    // Get the link that the user entered
+    const inputValue = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].value;
 
-      // Get the link that the user entered
-      const inputValue = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].value;
+    // Get the input element
+    const input = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0];
 
-      // Get the input element
-      const input = e.target.parentElement.parentElement.childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0];
+    // Create date submission expires
+    const expireDate = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000);
 
-      // Create date submission expires
-      const expireDate = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000);
-
-      // Create updated value object
-      const updatedValue = {
-        [inputId]: inputValue,
-        [inputId + 'Expires']: expireDate
-      }
-
-      // Update on firebase
-      const docRef = doc(firestore, currentUser.team, currentUser.id, "links", currentUser.id);
-      updateDoc(docRef, updatedValue);
-
-      // Reset input element value after submitting
-      input.value = '';
+    // Create updated value object
+    const updatedValue = {
+      [inputId]: inputValue,
+      [inputId + 'Expires']: expireDate
     }
 
+    // Update on firebase
+    const docRef = doc(firestore, this.props.currentUser.team, this.props.currentUser.id, "links", this.props.currentUser.id);
+    updateDoc(docRef, updatedValue);
+
+    this.setState({
+      ...updatedValue
+    });
+
+    // Reset input element value after submitting
+    input.value = '';
+  }
+
+  render() {
+    const { agilityLink, broadLink, fortyLink, threeLink, verticalLink, wbLink, wbLinkExpires, threeLinkExpires, broadLinkExpires, verticalLinkExpires, agilityLinkExpires, fortyLinkExpires } = this.state;
+  
     const today = new Date().getTime() / 1000;
 
     return (
@@ -76,22 +79,22 @@ class ProfileHsLinkGrid extends React.Component {
         <></>
         :<>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="Wall Ball Link" data={wbLink} handleSubmit={handleSubmit} dataId="wbLink" expires={wbLinkExpires ? Math.round((wbLinkExpires.seconds - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="Wall Ball Link" data={wbLink} handleSubmit={this.handleSubmit} dataId="wbLink" expires={wbLinkExpires ? Math.round((wbLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="300's Link" data={threeLink} handleSubmit={handleSubmit} dataId="threeLink" expires={threeLinkExpires ? Math.round((threeLinkExpires.seconds - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="300's Link" data={threeLink} handleSubmit={this.handleSubmit} dataId="threeLink" expires={threeLinkExpires ? Math.round((threeLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="Broad Jump Link" data={broadLink} expires={broadLinkExpires ? Math.round((broadLinkExpires.seconds - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="Broad Jump Link" data={broadLink} handleSubmit={this.handleSubmit} dataId="broadLink" expires={broadLinkExpires ? Math.round((broadLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="Vertical Jump Link" data={verticalLink} expires={verticalLinkExpires ? Math.round((verticalLinkExpires - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="Vertical Jump Link" data={verticalLink} handleSubmit={this.handleSubmit} dataId="verticalLink" expires={verticalLinkExpires ? Math.round((verticalLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="5-10-5 Link" data={agilityLink} expires={agilityLinkExpires ? Math.round((agilityLinkExpires - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="5-10-5 Link" data={agilityLink} handleSubmit={this.handleSubmit} dataId="agilityLink" expires={agilityLinkExpires ? Math.round((agilityLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
           <Grid item xs={12} sx={{textAlign: 'left', mt: 2}}>
-            <ProfileHsLink dataTitle="40yd Dash Link" data={fortyLink} expires={fortyLinkExpires ? Math.round((fortyLinkExpires - today) / 86400) : undefined} />
+            <ProfileHsLink dataTitle="40yd Dash Link" data={fortyLink} handleSubmit={this.handleSubmit} dataId="fortyLink" expires={fortyLinkExpires ? Math.round((fortyLinkExpires.seconds - today) / 86400) : undefined} />
           </Grid>
         </>
   
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -79,18 +79,10 @@ export const createUserProfileDocument = async (userAuth, displayName, team, gra
       }
     }
   }
-  
-  console.log(userRef);
   return userRef;
 }
 
-
-
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
-
-// const provider = new firebase.auth.GoogleAuthProvider();
-// provider.setCustomParameters({prompt: 'select_account'});
-// export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
